9ac4444e81c0b33df7a203347c79826eb975eebb
steel1990 <lbbsteel@gmail.com>
2016-01-19 19:54:13 +0800

    add traceroute

--- ip.js
+++ ip.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var bl = require("bl");
+var exec = require('child_process').exec;
 
 exports.getCurrentIp = function(){
     /**
@@ -28,7 +29,7 @@ exports.getCurrentIp = function(){
     return result;
 };
 
-exports.getAddress = function (ip) {
+exports.getAddress = function (ip, callback) {
     http.get('http://ip.taobao.com/service/getIpInfo.php?ip=' + ip, function (res) {
         res.pipe(bl(function (err, data) {
             if (err) {
@@ -36,9 +37,52 @@ exports.getAddress = function (ip) {
                 return;
             }
             data = JSON.parse(data).data;
-            console.log(ip, data.country || '', data.region || '', data.isp || '');
+            if (callback) {
+                callback(data.country + ' ' + data.region + ' ' + data.isp);
+            } else {
+                console.log(ip, data.country || '', data.region || '', data.isp || '');
+            }
         }));
     }).on('error', function(e) {
-        console.log("Got error: " + e.message);
+        if (callback) {
+            callback('Unknow');
+        } else {
+            console.log("Got error: " + e.message);
+        }
+    });
+};
+
+var getAddressPromise = function (ip) {
+    // console.log('get', ip);
+    return new Promise((resolve, reject) => {
+        exports.getAddress(ip, function (address) {
+            // console.log('addr', address, ip);
+            resolve(address);
+        });
+    });
+};
+
+
+var IP_REG = /((?:\d+\.){3}\d+)\s+\(\1\)/;
+exports.traceRoute = function (domain) {
+    var trace = exec('traceroute ' + domain);
+
+    var seq = Promise.resolve();
+
+    trace.stdout.on('data', function (data) {
+        // 220.181.17.90 (220.181.17.90)
+        if (IP_REG.test(data)) {
+            seq = seq.then(() => {
+                return getAddressPromise(RegExp.$1).then((addr) => {
+                    data = data.replace(IP_REG, '$1 (' + addr + ')');
+                    process.stdout.write(data);
+                });
+            });
+        } else {
+            seq = seq.then(() => process.stdout.write(data));
+        }
+    });
+    trace.stderr.on('data', function (data) {
+        process.stdout.write(data);
     });
 };
